8768384ffd74ba48872c155d2f6c56aa61c03766
marcoking13 <marcokhodr16@gmail.com>
2023-12-19 10:44:19 -0700

    l

---
+++ app.js
@@ -0,0 +1,21 @@
+var express = require("express");
+var path = require("path");
+var port = 4002;
+
+
+var app = express();
+app.use(express.static(__dirname));
+app.use(express.static(path.join(__dirname, "assets")));
+
+app.get("/",(req,res)=>{
+  res.sendFile(path.join(__dirname,"index.html"));
+});
+
+app.get("/fight",(req,res)=>{
+  res.sendFile(path.join(__dirname,"fight.html"));
+});
+
+
+app.listen(port,()=>{
+  console.log("listening");
+})
